03675d0f1fd9a5f3f1c78119228a1ba5aecbf345
Dipak-007 <116078188+Dipak-007@users.noreply.github.com>
2022-10-18 18:47:37 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+//program to reverse a number
+class Reverse{
+	public static void main(String arg[]){
+		int n=Integer.parseInt(arg[0]);
+		int t=n,rev=0,r;
+		while(t!=0){
+			r=t%10;
+			rev=rev*10+r;
+			t/=10;
+		}
+		System.out.print("Reverse of "+n+" = "+rev);
+	}
+}
